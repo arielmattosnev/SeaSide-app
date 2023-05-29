e1eef10858e0befe264687b2dce594a6ac547346
Ariel Mattos <arielmattosnev@gmail.com>
2023-05-29 01:44:41 -0300

    feat: add all necassary texts to the project

---
+++ src/constants/index.js
@@ -0,0 +1,88 @@
+import maldives from "../assets/maldives.jpg";
+import boraBora from "../assets/borabora.jpg";
+import keywest from "../assets/keywest.jpg";
+import boraBora2 from "../assets/borabora2.jpg";
+import maldives2 from "../assets/maldives2.jpg";
+import maldives3 from "../assets/maldives3.jpg";
+
+export const navbarLinks = [
+  {
+    id: 1,
+    content: "home",
+  },
+  {
+    id: 2,
+    content: "destinos",
+  },
+  {
+    id: 3,
+    content: "viajar",
+  },
+  {
+    id: 4,
+    content: "visualizar",
+  },
+  {
+    id: 5,
+    content: "reserva",
+  },
+];
+
+export const buttonText = [
+  {
+    id: 1,
+    text: "pesquisar",
+  },
+  {
+    id: 2,
+    text: "conta",
+  },
+];
+
+export const selectsImages = [
+  {
+    id: 1,
+    name: "Maldivas",
+    href: maldives,
+  },
+  {
+    id: 2,
+    name: "Bora Bora",
+    href: boraBora,
+  },
+  {
+    id: 3,
+    name: "Keywest",
+    href: keywest,
+  },
+  {
+    id: 4,
+    name: "Cozmel",
+    href: boraBora2,
+  },
+  {
+    id: 5,
+    name: "Jamaica",
+    href: maldives2,
+  },
+  {
+    id: 6,
+    name: "Antigua",
+    href: maldives3,
+  },
+];
+
+export const carouselSlider = [
+  {
+    id: 0,
+    url: "https://images.unsplash.com/photo-1515238152791-8216bfdf89a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80",
+  },
+  {
+    id: 1,
+    url: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80",
+  },
+  {
+    id: 2,
+    url: "https://images.unsplash.com/photo-1510414842594-a61c69b5ae57?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+  },
+];
