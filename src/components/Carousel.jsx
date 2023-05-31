f5ddbb505ab69f7b35f04a318035d9ed88e5800b
Ariel Mattos <arielmattosnev@gmail.com>
2023-05-31 16:58:58 -0300

    fix: fixed thr carousel slider

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,24 +3,25 @@ import { carouselSlider } from "../constants";
 import { BsArrowLeftSquareFill, BsArrowRightSquareFill } from "react-icons/bs";
 
 const Carousel = () => {
-  const [slide, setSlide] = useState(1);
+  const [slide, setSlide] = useState(0);
 
   const prevSlide = () => {
-    setSlide(slide === carouselSlider.length - 1 ? 0 : slide);
+    setSlide((slide + carouselSlider.length - 1) % carouselSlider.length);
   };
+
   const nextSlide = () => {
-    setSlide(slide === 0 ? carouselSlider.length - 1 : slide - 1);
+    setSlide((slide + 1) % carouselSlider.length);
   };
 
   return (
     <section className="max-w-[1240px] mx-auto px-4 py-16 relative flex items-center justify-center">
       <BsArrowLeftSquareFill
         onClick={prevSlide}
-        className="absolute top-[50%] text-3xl fill-white cursor-pointer left-8 "
+        className="absolute top-[50%] text-3xl fill-white cursor-pointer left-8"
       />
       <BsArrowRightSquareFill
         onClick={nextSlide}
-        className="absolute top-[50%] text-3xl fill-white cursor-pointer right-8 "
+        className="absolute top-[50%] text-3xl fill-white cursor-pointer right-8"
       />
       {carouselSlider.map(({ id, url }) => (
         <div
